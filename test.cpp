4e6a1e7a996302789837dadc3f68b934bec6c6e4
Qian Huang <cianhuang@outlook.com>
2018-01-08 14:42:15 +0800

    add a test cpp file add a test cpp file

---
+++ test.cpp
@@ -0,0 +1,17 @@
+// test.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	a = 5;
+	b = 10;
+	std::cout << a + b << std::endl; //add some changes to see how Github works.
+	system("PAUSE");
+    return 0;
+}
+
