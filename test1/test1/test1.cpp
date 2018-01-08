dbf1eb7c20bb6ac30f6516a3eb9c106583db8893
Qian Huang <cianhuang@outlook.com>
2018-01-08 14:56:04 +0800

    test project add one project on ADD PROGRAMME

---
+++ test1/test1/test1.cpp
@@ -0,0 +1,16 @@
+// test1.cpp: ADD
+// test github
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << a + b << endl;
+	system("pause");
+    return 0;
+}
+
