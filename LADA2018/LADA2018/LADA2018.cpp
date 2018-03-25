4997eca287ad30838ed2127efc6d7afd730c176f
Qian Huang <cianhuang@outlook.com>
2018-03-25 20:56:28 +0800

    update

--- LADA2018/LADA2018/LADA2018.cpp
+++ LADA2018/LADA2018/LADA2018.cpp
@@ -1,115 +1,89 @@
-//问题 B : 所有的逆序对
-//时间限制 : 2 Sec  内存限制 : 5 MB
-//	提交 : 313  解决 : 53
+//问题 A : 最大子序列和问题
+//时间限制 : 1 Sec  内存限制 : 4 MB
+//	提交 : 194  解决 : 72
 //
 //	题目描述
-//	请大家在做oj题之前，仔细阅读关于抄袭的说明http ://www.bigoh.net/JudgeOnline/.
+//	给定一整数序列 a1, a2, …, an，求 a1~an 的一个子序列 ai~aj，使得从 ai 到 aj 的和最大。
+//	只需要求出最大子序列的和，而不需要求出最大的那个序列。
 //
+//	输入
+//	一组整数，数字和数字之间以空格隔开。
+//	输出
+//	该整数序列中最大子序列的和
+//	样例输入
+//	- 2 11 - 4 13 - 5 - 2
+//	样例输出
+//	20
+//	提示
 //
-//给出一个字符串数组，如果（按照字典序）一个大的字符串在比它小的字符串前面我们称这两个字符串组成一个“逆序对”。你需要找到所有的逆序对的个数。
-//
-//输入
-//第一行是数组大小，第二行是以空格分隔的字符串数组.
-//
-//注：预先知道每个字符串的长度都是 10.
-//
-//输出
-//所有的逆序对个数.这次需要大家先输出一个字符串，它是“我已阅读关于抄袭的说明”的汉语拼音.输出此行的提交我们将认为已经完全阅读并了解了“关于抄袭的说明”公告.
-//
-//注意：结果比较大，请用 long 类型保存.
-//
-//样例输入
-//3
-//aaaaaaaaaa cccccccccc bbbbbbbbbb
-//样例输出
-//wo yi yue du guan yu chao xi de shuo ming
-//1
-//提示
-//不要使用蛮力算法
+//	应用穷举法可以得到 O(n3) 的算法，优化它即可得到 O(n2) 的算法。这两个算法将会超时。
+//	利用分治的思想可以有 O(n*logn) 的算法。
+//	也有聪明的算法，它的复杂度是 O(n) 的。
+//	另请注意：输入多少个数是未知的。请思考如何处理。
 
 #include "stdafx.h"
 #include <iostream>
-#include <string>
+#include <vector>
 using namespace std;
 
-long countInverse(string *, long);
-long InvBetLeftRight(string *, long);
-void quickSort(string *, long);
-long partition(string *, string, long);
-void output(string *, long);
-
-// O(n*loglong(n))
 int main()
 {
-	long n; // if told to use 'long', then use it.
-	string *arr;
-	cin >> n;
-	arr = new string[n];
-	for (long i = 0; i < n; ++i) {
-		cin >> arr[i];
+	vector<int> arr;
+	int num;
+	while (cin >> num) {
+		arr.push_back(num);
 	}
-	long count = countInverse(arr, n);
-	cout << "wo yi yue du guan yu chao xi de shuo ming" << endl;
-	cout << count << endl;
-
-	delete[]arr;
-	system("pause");
-	return 0;
-}
-
-long countInverse(string *arr, long n) {
-	if (n == 1) return 0;
-	long count = countInverse(arr, n / 2) + countInverse(arr + n / 2, n - n / 2);
-	count += InvBetLeftRight(arr, n); // !!! if placed ahead, sort function would shuffle
-										// the array, and cause destructive result.
-	return count;
-}
-
-long InvBetLeftRight(string *arr, long n) {
-	quickSort(arr, n / 2);
-	quickSort(arr + n / 2, n - n / 2);
-	long left = 0, right = n / 2;
-	long count = 0;
-	while (left < n / 2) {
-		if (arr[left] > arr[right]) {
-			while (right < n && arr[left] > arr[right]) {
-				count += (n / 2 - left);
-				++right;
-			}
-			if (right == n) break; // gauantee it wont visit arr[n].
+	int n = arr.size();
+	int pointer = 0, tempSum = 0;
+	int bigNeg = INT32_MIN; //prevent all inputs are negative.
+	for (int i = 0; i < n - 1; ++i) {
+		if (arr[i] < 0 && arr[i] > bigNeg)
+			bigNeg = arr[i];
+		if (arr[i] == 0) continue;
+		tempSum += arr[i];
+		if (arr[i] * arr[i + 1] <= 0) { // deal with 0 --- 0 1 2 3 0 -1 -2 0 -3 1 2 3 -1
+			if (pointer > 0 && tempSum * arr[pointer - 1] > 0)
+				arr[pointer - 1] += tempSum;
+			else 
+				arr[pointer++] = tempSum;
+			tempSum = 0;
 		}
-		++left;
 	}
-	return count;
-}
+	if ((tempSum + arr[n - 1]) != 0)
+		arr[pointer++] = tempSum + arr[n - 1];
 
-void output(string *arr, long n) {
-	for (long i = 0; i < n - 1; ++i) {
-		cout << arr[i] << ' ';
+	for (int i = 0; i < n - pointer; ++i)
+		arr.pop_back();
+
+	int Max = 0, tempMax = 0, i = 0;
+	if (arr.size() != 0 && arr[arr.size() - 1] < 0)
+		arr.pop_back();
+
+	//for (auto it = arr.cbegin(); it != arr.cend(); ++it)
+	//	std::cout << *it << ' ';
+
+	n = arr.size();
+	while (i < n)
+	{
+		if (i == 0 && arr[i] < 0) {
+			++i;
+			continue;
+		}
+		tempMax = arr[i];
+		while (i + 2 < n && arr[i + 1] + arr[i + 2] >= 0) {
+			tempMax += (arr[i + 1] + arr[i + 2]);
+			i += 2;
+		} // Why there is no further margin constraint?
+
+		if (Max < tempMax)
+			Max = tempMax;
+		i += 2;
 	}
-	cout << arr[n - 1] << endl;
-}
+	if (n == 0 && bigNeg != INT32_MIN)
+		std::cout << bigNeg << endl;
+	else
+		std::cout << Max << endl;
 
-void quickSort(string *arr, long n) {
-	if (n == 1 || n == 0) return;
-	string pivot = arr[n / 2];
-	string temp = arr[0]; 
-	arr[0] = arr[n / 2]; arr[n / 2] = temp;
-	long k = partition(arr, pivot, n);
-	quickSort(arr, k);
-	quickSort(arr + k + 1, n - k - 1);
-}
-
-long partition(string *arr, string pivot, long n) {
-	long head = 0, rear = n - 1;
-	string temp;
-	while (head < rear) {
-		while (arr[rear] > pivot) 
-			--rear;
-		temp = arr[head]; arr[head] = arr[rear]; arr[rear] = temp;
-		while (arr[head] <= pivot && head < rear) 
-			++head;
-		temp = arr[rear]; arr[rear] = arr[head]; arr[head] = temp;
-	}
-	return head;
+	std::system("PAUSE");
+	return 0;
 }
