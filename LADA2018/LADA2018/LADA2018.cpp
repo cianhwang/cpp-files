77c3a3d71fb68774391b605f4b57aac5eb654150
Qian Huang <cianhuang@outlook.com>
2018-05-25 23:36:30 +0800

    --

--- LADA2018/LADA2018/LADA2018.cpp
+++ LADA2018/LADA2018/LADA2018.cpp
@@ -10,61 +10,64 @@ public:
 	vector<int> neighbors;
 };
 
-void BFSsweep(vector<Node> List, int n, int v);
-void BFS(vector<Node> List, char *color, int v, int parent, queue<int> &q);
+void BFSsweep(vector<Node> List, int n, int v, bool *twoColor);
+void BFS(vector<Node> List, char *color, int v, bool *twoColor, queue<int> &q);
 
 int main()
 {
 	vector<Node> vec;
-	Node Vertex;
 	string str;
 	while (getline(cin, str)) {
+		Node Vertex;
 		for (auto i = 1; i < str.length(); ++i)
 			if (str[i] != ' ') {
 				Vertex.neighbors.push_back(str[i] - '0');
 			}
 		vec.push_back(Vertex);
 	}
-	cout << "wo yi yue du guan yu chao xi de shuo ming" << endl;
-	BFSsweep(vec, vec.size(), 0);
+	int n = vec.size();
+	bool *twoColor = new bool[n];
+	BFSsweep(vec, n, 0, twoColor);
 	// output
-
+	cout << "wo yi yue du guan yu chao xi de shuo ming" << endl;
+	for (int i = 0; i < n; ++i) {
+		if (twoColor[i])
+			cout << i << endl;
+	}
 	system("pause");
 	return 0;
 }
 
-void BFSsweep(vector<Node> List, int n, int v) {
+void BFSsweep(vector<Node> List, int n, int v, bool *twoColor) {
 	char *color = new char[n];
 	for (int i = 0; i < n; ++i) { color[i] = 'w'; }
 	queue<int> q;
 	for (int i = 0; i < n; ++i) {
 		if (color[(v + i) % n] == 'w')
-			BFS(List, color, v + i, 0, q);
+			BFS(List, color, v + i, twoColor, q);
 	}
 	return;
 }
 
-void BFS(vector<Node> List, char *color, int v, int parent, queue<int> &q) {
-	int w;
+void BFS(vector<Node> List, char *color, int v, bool *twoColor, queue<int> &q) {
 	vector<int> remAdj;
 	q.push(v);
 	// Pre Processing
-
+	twoColor[v] = true;
 	color[v] = 'g';
 	while (!q.empty()) {
-		w = q.front(); q.pop();
+		int w = q.front(); q.pop();
 		remAdj = List[w].neighbors;
 		while (!remAdj.empty()) {
-			int x = remAdj.front();
+			int x = remAdj.back();
 			if (color[x] == 'w') {
-				q.push(x); color[x] = 'g';
+				q.push(x); color[x] = 'g'; twoColor[x] = !(twoColor[w]);
 			}
-			else if (color[x] == 'g') // MOST CRUCIAL PART!!!!
-				;
-			remAdj.erase(remAdj.begin());
+			//else if (color[x] == 'g') // MOST CRUCIAL PART!!!!
+			//	;
+			remAdj.pop_back();
 		}
 		// Post Processing
-		parent = 1 - parent;//...................
 		color[w] = 'b';
 	}
 	return;
